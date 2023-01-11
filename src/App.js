145e7686690b3e8dfe2acbac1b1816141408a929
Justus <justus.onyancha@moringaschool.com>
2023-01-11 14:17:55 +0300

    add props and components in other components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+export default function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Comment username="Princess" time={(new Date()).toString()}>
+      <p>Hello My Love</p>
+      <p>This is a comment according to how we defined our children in our Comment component</p>
+
+    </Comment>
   );
 }
 
-export default App;
+
+function Comment ({username, time, children}){
+  return(
+    <section>
+      <p>{username} commented at {time}</p>
+      {children}
+    </section>
+  );
+}
